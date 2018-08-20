8abdd67c9afb4aaba1dd845cadbcfb7118589bec
tangchuanxi <383258210@qq.com>
2018-08-20 17:41:32 +0800

    首页更改

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
     res.sendfile("./public/index.html");
   });
-app.listen(8090, function() {
- console.log('App listening at port 8090;');
+app.listen(8099, function() {
+ console.log('App listening at port 8099;');
 });
