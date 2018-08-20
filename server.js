c65edf2bc4f35e7d61bc2576b394e71c83a2896a
tangcx <383258210@qq.com>
2018-08-20 22:16:21 +0800

    详情页

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
  
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function(req, res) {
-    res.sendfile("./public/index.html");
+    res.sendfile("./public/detail.html");
   });
 app.listen(8099, function() {
  console.log('App listening at port 8099;');
